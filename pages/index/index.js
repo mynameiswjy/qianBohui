d8e82187f5c7714dc71407ab8c15a12bffe6cb0d
wjy <wjy631438519@163.com>
2018-08-19 22:54:47 +0800

    22

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,60 +1,39 @@
-// pages/index/index.js
+var app = getApp()
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    array: ['美国', '中国', '巴西', '日本'],
+    valHot: '热门',
+    indexHot: ''
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
   
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  hotSelect(e) {
+    var index = e.detail.value
+    console.log(typeof index);
+    this.setData({
+      valHot: this.data.array[index],
+      indexHot: index
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
   },
 
   /**
